6a748d27b1aba6351d5240d71ce78eb633ccf3ab
Xiaoqi Jiang <kjad123@yahoo.com>
2026-03-23 16:42:12 -0500

    Creat load_to_postgres.py to load the data to postgresql

---
+++ scripts/load_to_postgres.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Load cleaned CSV
+df = pd.read_csv("data/cleaned_listings.csv")
+
+# Connect to PostgreSQL
+engine = create_engine("postgresql+psycopg2://Gin@localhost:5433/airbnb")
+
+# Load data into the listings table
+df.to_sql("listings", engine, if_exists="append", index=False)
+
+print("Data loaded into PostgreSQL successfully.")
